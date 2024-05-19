c085cb0d9abb26964d2eb49ae58b0dcf2ff4347b
Dziroz <abcdanil@bk.ru>
2024-05-19 19:24:30 +0500

    pohui

---
+++ Assets/Scripts/bossHP.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+
+namespace TopDownShooter
+{
+    public class bossHP : MonoBehaviour
+    {
+        public int HP;
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(HP <= 0)
+            {
+                //SceneManager.LoadScene();
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.GetComponent<Damage>())
+            {
+                HP--;
+            }
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.transform.GetComponent<Damage>())
+            {
+                HP--;
+            }
+        }
+    }
+}
