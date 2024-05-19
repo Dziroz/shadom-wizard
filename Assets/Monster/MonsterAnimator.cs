60af244c929840a94be9adb159680128627fafd6
Dziroz <abcdanil@bk.ru>
2024-05-19 14:05:30 +0500

    IPMACT

---
+++ Assets/Monster/MonsterAnimator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAnimator : MonoBehaviour
+{
+    public Animator anim;
+    private GameObject Player;
+    public float Dist_Player;
+
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Dist_Player = Vector3.Distance(Player.transform.position, gameObject.transform.position);
+        if (Dist_Player < 2)
+        {
+            anim.SetBool("isAttack", true);
+        }
+        else
+        {
+            anim.SetBool("isAttack", false);
+        }
+    }
+}
